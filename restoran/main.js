b1ea8df9db41220be9e4824fa1c9e0bd9fa2aac6
Ahmad Fashich Azzuhri Ramadhani <149882531+Fashich@users.noreply.github.com>
2024-06-30 21:40:11 +0700

    Add files via upload

---
+++ restoran/main.js
@@ -0,0 +1,7 @@
+var typed= new typed(".teks-selamat-datang", {
+    Strings:["Selamat Datang🤗!!!", "Anda Lapar? cobalah Paket Gacor", "Es Cao Susu? byuhh bikin nagihh", "Welcome to Kelompok 14"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
